68a63a45a5a8e03f2b4b6003310ba7a430bac2ae
Edigiraldo <edison.giraldoa12@gmail.com>
2022-09-10 09:50:23 -0500

    Include abstract repository for exams storage

--- repository/repository.go
+++ repository/repository.go
@@ -9,6 +9,8 @@ import (
 type Repository interface {
 	GetStudent(ctx context.Context, id string) (*models.Student, error)
 	SetStudent(ctx context.Context, student *models.Student) error
+	GetExam(ctx context.Context, id string) (*models.Exam, error)
+	SetExam(ctx context.Context, exam *models.Exam) error
 }
 
 var implementation Repository
@@ -23,4 +25,12 @@ func GetStudent(ctx context.Context, id string) (*models.Student, error) {
 
 func SetStudent(ctx context.Context, student *models.Student) error {
 	return implementation.SetStudent(ctx, student)
+}
+
+func GetExam(ctx context.Context, id string) (*models.Exam, error) {
+	return implementation.GetExam(ctx, id)
+}
+
+func SetExam(ctx context.Context, exam *models.Exam) error {
+	return implementation.SetExam(ctx, exam)
 }
