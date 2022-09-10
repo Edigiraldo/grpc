d5acb0a8188b4790186f2a50e4d52567362d45d9
Edigiraldo <edison.giraldoa12@gmail.com>
2022-09-10 11:39:03 -0500

    Include Question model

--- models/models.go
+++ models/models.go
@@ -10,3 +10,10 @@ type Exam struct {
 	Id   string `json:"id"`
 	Name string `json:"name"`
 }
+
+type Question struct {
+	Id       string `json:"id"`
+	Question string `json:"question"`
+	Answer   string `json:"answer"`
+	ExamId   string `json:"exam_id"`
+}
